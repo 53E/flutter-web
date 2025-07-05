f46ab8da717f4949866e9311d054f7615a58308e
jin <jinwoo3236@naver.com>
2025-07-05 21:08:24 +0900

    Add: Pre-built Flutter web files for deployment

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1655713319"
+    serviceWorkerVersion: "2615202698"
   }
 });
